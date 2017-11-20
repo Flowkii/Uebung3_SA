92b0ef051d0546a71669b102711f27a75d19ab2d
Flowkii <florian.braun@students.fhv.at>
2017-11-20 11:27:39 +0100

    loadImage

---
+++ src/LoadImage.java
@@ -0,0 +1,58 @@
+import javax.media.jai.JAI;
+import javax.media.jai.PlanarImage;
+import java.io.Serializable;
+import java.util.Vector;
+
+public class LoadImage implements Serializable {
+
+    private String path;
+    private Vector listeners = new Vector();
+    private PlanarImage planarImage;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+        run();
+    }
+
+    public void addImageAppearanceListener(ImageAppearanceListener listener) {
+        listeners.addElement(listener);
+
+    }
+
+    public void removeImageAppearanceListener(ImageAppearanceListener listener) {
+        listeners.removeElement(listener);
+
+    }
+
+    public void run() {
+
+        planarImage = JAI.create("fileload", path);
+        fireImageAppearanceEvent();
+
+
+    }
+
+    protected synchronized void fireImageAppearanceEvent() {
+        synchronized (this) {
+        }
+
+        Vector var1;
+        try {
+            var1 = (Vector) this.listeners.clone();
+        } catch (Throwable var6) {
+            throw var6;
+        }
+
+        ImageAppearanceEvent var2 = new ImageAppearanceEvent(this, this.planarImage);
+
+        for (int var3 = 0; var3 < var1.size(); ++var3) {
+            ImageAppearanceListener var4 = (ImageAppearanceListener) var1.elementAt(var3);
+            var4.imageAppearanceChanged(var2);
+        }
+
+    }
+}
---
+++ src/LoadImageBeanInfo.java
@@ -0,0 +1,38 @@
+import java.beans.EventSetDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+public class LoadImageBeanInfo extends SimpleBeanInfo {
+
+    @Override
+    public EventSetDescriptor[] getEventSetDescriptors() {
+        Class imgClass = LoadImage.class;
+        String imageSource = "imageSource";
+        Class listenerClass = ImageAppearanceListener.class;
+        String[] var5 = new String[]{"imageAppearanceChanged"};
+        String addListener = "addImageAppearanceListener";
+        String removeListener="removeImageAppearanceListener";
+        EventSetDescriptor var1 = null;
+        try {
+            var1 = new EventSetDescriptor(imgClass, imageSource, listenerClass, var5, addListener, removeListener);
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+        }
+        EventSetDescriptor[] result = new EventSetDescriptor[]{var1};
+        return result;
+    }
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        Class var=LoadImage.class;
+        try {
+            PropertyDescriptor prop1=new PropertyDescriptor("imageSource",var);
+            PropertyDescriptor[] prop2 = new PropertyDescriptor[]{prop1};
+            return prop2;
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
